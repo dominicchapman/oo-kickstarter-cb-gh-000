ab51b0031908e575fbf73bed017dc3aa8920742b
Dominic Chapman <dominicchapman@me.com>
2017-10-11 22:22:12 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,16 @@
 
 class Backer
 
+  @@backed_projects = []
+  
   attr_accessor :name
 
   def initialize(name)
     @name = name
-    @@backed_projects = []
+  end
+
+  def backed_projects
+    @@backed_projects
   end
 
   def back_project(project)
