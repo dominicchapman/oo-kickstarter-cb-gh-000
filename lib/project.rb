9adc7268a44d4a77a8a609054216da78ad4ec586
Dominic Chapman <dominicchapman@me.com>
2017-10-11 22:25:01 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -15,6 +15,8 @@ class Project
 
   def add_backer(backer)
     self.backers << backer
+    # add the project to the backer's backed_projects array
+    backer.backed_projects << self unless backer.backed_projects.include?(self)
   end
 
 end
